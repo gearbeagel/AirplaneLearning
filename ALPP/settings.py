d22a847d8db2cf2fd62dfa846cbb1aa1ffb846cb
Vic K <kondratskayavictoria@gmail.com>
2024-03-16 23:39:17 +0200

    goofing around with dbs

--- ALPP/settings.py
+++ ALPP/settings.py
@@ -110,12 +110,25 @@ WSGI_APPLICATION = 'ALPP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'PRODUCTION' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'AirplaneLearningPolyProDB',
+            'USER': 'gearbeagel',
+            'PASSWORD': 'Funnyhaha111',
+            'HOST': 'alpp-server.database.windows.net',
+            'PORT': '3306',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
