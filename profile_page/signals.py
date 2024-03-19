4750da8dee55cce2cbd26ebcf5a73d52e4040c37
Vic K <kondratskayavictoria@gmail.com>
2024-03-19 14:01:45 +0200

    played around with db

--- profile_page/signals.py
+++ profile_page/signals.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+from modules.models import Lesson
 from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        lessons = Lesson.objects.all()
+        for lesson in lessons:
+            lesson.status = "Not Started"
+            lesson.save()
