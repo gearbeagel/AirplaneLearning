45b015ccfdc7eddc96846d3aa91f607a48277032
Vic K <kondratskayavictoria@gmail.com>
2024-03-19 14:18:43 +0200

    played around with db

--- profile_page/signals.py
+++ profile_page/signals.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from modules.models import Lesson
+from modules.models import Lesson, Quiz
 from .models import Profile
 
 @receiver(post_save, sender=User)
@@ -10,6 +10,10 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
         lessons = Lesson.objects.all()
+        quizzes = Quiz.objects.all()
         for lesson in lessons:
             lesson.status = "Not Started"
             lesson.save()
+        for quiz in quizzes:
+            quiz.status = "Not Started"
+            quiz.save()
