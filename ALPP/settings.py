441d4bb10ac5e25b3fdce76b213f1a8e5eebb2f6
Vic K <kondratskayavictoria@gmail.com>
2024-03-21 22:24:42 +0200

    made minor changes

--- ALPP/settings.py
+++ ALPP/settings.py
@@ -122,7 +122,8 @@ WSGI_APPLICATION = 'ALPP.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-DATABASES = {
+if 'TESTING' in os.environ:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'alpp',
@@ -132,7 +133,13 @@ DATABASES = {
             'PORT': '3306',
         }
     }
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
