8660295893c8f28c7f00c02d89e8a9043b6de086
Vic K <kondratskayavictoria@gmail.com>
2024-04-16 14:21:09 +0300

    removed redoc

--- ALPP/urls.py
+++ ALPP/urls.py
@@ -17,11 +17,6 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
-import discussion_forums.views
 import profile_page.views
 import registration_handle.views
 from ALPP import settings
@@ -31,7 +26,6 @@ urlpatterns = ([
     path('admin/', admin.site.urls),
     path('api/', include('modules.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('get_user_info/', profile_page.swagger.get_user_info),
     path('',  include('registration_handle.urls')),
     path("accounts/", include("allauth.urls")),
