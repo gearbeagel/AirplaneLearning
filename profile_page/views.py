1a6e2569ae517fb143647c8913cac4005b6379bc
Vic K <kondratskayavictoria@gmail.com>
2024-05-14 21:30:57 +0300

    added dependency + exception tracing

--- profile_page/views.py
+++ profile_page/views.py
@@ -11,11 +11,15 @@ from rest_framework import permissions, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
+from discussion_forums.utils import load_profanity_words
 from profile_page.forms import LearnerTypeSettings, ProfilePictureSettings, NotificationSettings
 from profile_page.models import Profile, get_random_profile_pic, LearnerType
 from modules.models import Lesson, Quiz, Module
 from modules.user_progress_models import LessonStatus, QuizStatus, QuizUserAnswers
 from profile_page.swagger import StudentSerializer
+
+PROFANE_WORDS = load_profanity_words('profanity.txt')
+tracer = trace.get_tracer(__name__)
 
 
 def get_latest_lesson_and_quiz(profile):
@@ -49,32 +53,33 @@ def calculate_progress(user_profile, chosen_language_id):
 @api_view(['GET'])
 @permission_classes([permissions.IsAuthenticated])
 def profile_page(request, username):
-    student = get_student_profile(username)
-    if not student:
-        if 'setup' not in request.path:
-            return redirect("setup")
+    with tracer.start_as_current_span("profile_page") as span:
+        student = get_student_profile(username)
+        if not student:
+            if 'setup' not in request.path:
+                return redirect("setup")
 
-    calculate_progress(student, student.chosen_language_id)
+        calculate_progress(student, student.chosen_language_id)
 
-    latest_lesson, latest_lesson_language = get_latest_completed_lesson(student)
-    latest_quiz, latest_quiz_language = get_latest_completed_quiz(student)
+        latest_lesson, latest_lesson_language = get_latest_completed_lesson(student)
+        latest_quiz, latest_quiz_language = get_latest_completed_quiz(student)
 
-    profile_picture_url = get_profile_picture_url(student.profile_pic_url)
+        profile_picture_url = get_profile_picture_url(student.profile_pic_url)
 
-    serializer = StudentSerializer(student)
+        serializer = StudentSerializer(student)
 
-    data = {
-        'student': serializer.data
-    }
+        data = {
+            'student': serializer.data
+        }
 
-    if 'application/json' in request.META.get('HTTPS_ACCEPT', ''):
-        return Response(data, status=status.HTTP_200_OK)
-    else:
-        return render(request, 'profile_page.html', {'student': student, 'user': request.user,
-                                                     'latest_lesson': latest_lesson, 'latest_quiz': latest_quiz,
-                                                     'latest_lesson_language': latest_lesson_language,
-                                                     'latest_quiz_language': latest_quiz_language,
-                                                     'profile_picture_url': profile_picture_url})
+        if 'application/json' in request.META.get('HTTPS_ACCEPT', ''):
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            return render(request, 'profile_page.html', {'student': student, 'user': request.user,
+                                                         'latest_lesson': latest_lesson, 'latest_quiz': latest_quiz,
+                                                         'latest_lesson_language': latest_lesson_language,
+                                                         'latest_quiz_language': latest_quiz_language,
+                                                         'profile_picture_url': profile_picture_url})
 
 
 def get_student_profile(username):
@@ -109,53 +114,55 @@ def get_profile_picture_url(profile_pic_url):
 
 @login_required
 def profile_settings(request):
-    profile = Profile.objects.get(user=request.user)
-    learner_types = LearnerType.objects.all()
+    with tracer.start_as_current_span("profile_settings") as span:
+        profile = Profile.objects.get(user=request.user)
+        learner_types = LearnerType.objects.all()
 
-    if request.method == 'POST':
-        if 'learner_type_submit' in request.POST:
-            learner_type_form = LearnerTypeSettings(request.POST, instance=profile)
-            if learner_type_form.is_valid():
-                learner_type_form.save()
+        if request.method == 'POST':
+            if 'learner_type_submit' in request.POST:
+                learner_type_form = LearnerTypeSettings(request.POST, instance=profile)
+                if learner_type_form.is_valid():
+                    learner_type_form.save()
 
-        elif 'profile_pic_submit' in request.POST:
-            profile_pic_form = ProfilePictureSettings(request.POST, request.FILES, instance=profile)
-            if profile_pic_form.is_valid():
-                profile_pic_form.instance.container_name = 'pfpcontainer'
-                profile_pic_form.save()
+            elif 'profile_pic_submit' in request.POST:
+                profile_pic_form = ProfilePictureSettings(request.POST, request.FILES, instance=profile)
+                if profile_pic_form.is_valid():
+                    profile_pic_form.instance.container_name = 'pfpcontainer'
+                    profile_pic_form.save()
 
-        elif 'default_profile_pic' in request.POST:
-            profile.profile_pic_url = get_random_profile_pic()
-            profile.save()
+            elif 'default_profile_pic' in request.POST:
+                profile.profile_pic_url = get_random_profile_pic()
+                profile.save()
 
-        elif 'receive_notifications_submit' in request.POST:
-            data = request.POST.copy()
-            for field_name in [
-                'receive_notifications',
-                'new_modules_notifications',
-                'quiz_results_notifications',
-                'discussion_notifications',
-                'new_resources_notifications',
+            elif 'receive_notifications_submit' in request.POST:
+                data = request.POST.copy()
+                for field_name in [
+                    'receive_notifications',
+                    'new_modules_notifications',
+                    'quiz_results_notifications',
+                    'discussion_notifications',
+                    'new_resources_notifications',
 
-            ]:
-                data.setdefault(field_name, 'Do not send')
-            receive_notifications_form = NotificationSettings(data, instance=profile)
-            if receive_notifications_form.is_valid():
-                receive_notifications_form.save()
+                ]:
+                    data.setdefault(field_name, 'Do not send')
+                receive_notifications_form = NotificationSettings(data, instance=profile)
+                if receive_notifications_form.is_valid():
+                    receive_notifications_form.save()
 
-    learner_type_form = LearnerTypeSettings(instance=profile)
-    profile_pic_form = ProfilePictureSettings(instance=profile)
-    receive_notifications_form = NotificationSettings(instance=profile)
+        learner_type_form = LearnerTypeSettings(instance=profile)
+        profile_pic_form = ProfilePictureSettings(instance=profile)
+        receive_notifications_form = NotificationSettings(instance=profile)
 
-    return render(request, 'profile_settings.html', {
-        'learner_type_form': learner_type_form,
-        'profile_pic_form': profile_pic_form,
-        'receive_notifications_form': receive_notifications_form,
-        'learner_types': learner_types
-    })
+        return render(request, 'profile_settings.html', {
+            'learner_type_form': learner_type_form,
+            'profile_pic_form': profile_pic_form,
+            'receive_notifications_form': receive_notifications_form,
+            'learner_types': learner_types
+        })
 
 
 @login_required()
 def logout_page(request):
-    logout(request)
-    return redirect('/')
+    with tracer.start_as_current_span("logout_page") as span:
+        logout(request)
+        return redirect('/')
