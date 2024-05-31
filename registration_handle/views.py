26b46038b95b4bb466cd498a8b6b50f49a705a10
Vic K <kondratskayavictoria@gmail.com>
2024-06-01 01:08:32 +0300

    restyled the whole webapp

--- registration_handle/views.py
+++ registration_handle/views.py
@@ -42,28 +42,29 @@ def language_and_learning_path_selection(request):
     if Profile.objects.filter(user=request.user).exists():
         return redirect('profile_page', username=request.user.username)
 
-    if request.method != 'POST':
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            try:
+                Profile.objects.create(
+                    user=request.user,
+                    email=request.user.email,
+                    username=request.user.username,
+                    user_id=request.user.id,
+                    chosen_language=form_data['chosen_language'],
+                    learner_type=form_data['learner_type']
+                )
+                welcome_email(request)
+                return redirect('home')
+            except Exception as e:
+                print("Error occurred while creating profile:", e)
+                return HttpResponseBadRequest("Error occurred while creating profile. Please try again.")
+        else:
+            return HttpResponseBadRequest("Form submission failed. Please check your input.")
+    else:
         form = ProfileUpdateForm()
 
-    form = ProfileUpdateForm(request.POST)
-
-    if not form.is_valid():
-        return HttpResponseBadRequest("Form submission failed. Please check your input.")
-
-    form_data = form.cleaned_data
-    try:
-        Profile.objects.create(
-            user=request.user,
-            email=request.user.email,
-            username=request.user.username,
-            user_id=request.user.id,
-            chosen_language=form_data['chosen_language'],
-            learner_type=form_data['learner_type']
-        )
-        welcome_email(request)
-        return redirect('profile_page', username=request.user.username)
-    except Exception as e:
-        print("Error occurred while creating profile:", e)
     return render(request, 'home_registration/learning_path_choice.html', {'form': form})
 
 
